29e79a8307bf42289ff4857df3482deceb7629ce
Matthew Klawitter <mtklaw153@gmail.com>
2020-11-27 18:02:55 -0500

    Implemented n:m support for playlistsongs

--- src/controllers/db/playlists_controller.js
+++ src/controllers/db/playlists_controller.js
@@ -1,6 +1,5 @@
 /**
  * CRUD Playlist
- * TODO: Implement song m:m associations. We need to add a migration for fk's since sequelize won't auto do it
  */
 module.exports = function(app, db){
     app.post('/db/playlist', async (req, res) => {
@@ -15,8 +14,27 @@ module.exports = function(app, db){
             }
 
             // This playlist doesn't exist, we can safely create one
-            await db.Playlist.create(req.body);
+            let playlist = await db.Playlist.create(req.body);
             console.log("Succesfully POST playlist: " + req.body.name);
+
+            for (i = 0; i < req.body.songs.length; i++){
+                let song = await db.Songs.findOne({where: {id: songs[i]}});
+
+                // TODO: This might denote a race condition. Determine its removal before release
+                if (song != null){
+                    let entry = {
+                        playlistId: playlist.id,
+                        songId: songs[i]
+                    }
+    
+                    await db.PlaylistSongs.create(entry);
+                    console.log("Succesfully POST PlaylistSong: " + songs[i]);
+                }
+                else {
+                    console.log("Unable to POST PlaylistSong with id: " + songs[i] + ", no songs have the designated id.");
+                }
+            }
+            
             res.redirect('/admin/playlists');
         } catch (e){
             res.status(400).send(e.message);
@@ -27,10 +45,18 @@ module.exports = function(app, db){
         .get(async (req, res) => {
             try {
                 let playlist = await db.Playlist.findOne({where: {id: req.params.id}});
+                let playlistsongs = await db.PlaylistSongs.findAll({where: {playlistId: playlist.id}});
+                // for each song id in playlist songs we need to find the associated song id in the songs database.
+                let songs = await db.Songs.findAll({where: {id: playlistsongs.map((r) => {
+                    return r.id;
+                })}})
 
                 if (playlist != null){
-                    res.send(playlist);
-                    console.log("Sent GET playlist: " + req.params.id);
+                    res.send({
+                        playlist: playlist,
+                        songs: songs
+                    });
+                    console.log("Sent GET {playlist, songs}: " + req.params.id);
                     return;
                 }
                 else {
@@ -44,11 +70,38 @@ module.exports = function(app, db){
         })
         .put(async (req, res) => {
             try {
-                let playlist = await db.Playlist.find({where: {id: req.params.id}});
+                let playlist = await db.Playlist.findOne({where: {id: req.params.id}});
 
                 if (playlist != null){
                     playlist.name = res.body.name;
                     playlist.save();
+
+                    // Delete all records of PlaylistSongs with this playlist id
+                    let songs = await db.PlaylistSongs.findAll({where: {playlistId: playlist.id}});
+                    
+                    for (i = 0; i < songs.length; i++){
+                        await songs[i].destroy();
+                    }
+
+                    // Add in all new song records
+                    // TODO: This might denote a race condition. Determine its removal before release
+                    for (i = 0; i < req.body.songs.length; i++){
+                        let song = await db.Songs.findOne({where: {id: songs[i]}});
+        
+                        if (song != null){
+                            let entry = {
+                                playlistId: playlist.id,
+                                songId: songs[i]
+                            }
+            
+                            await db.PlaylistSongs.create(entry);
+                            console.log("Succesfully POST PlaylistSong with songId: " + songs[i]);
+                        }
+                        else {
+                            console.log("Unable to POST PlaylistSong with songId: " + songs[i] + ", no songs have the designated id.");
+                        }
+                    }
+
                     console.log("Successfully PUT playlist: " + req.params.id);
                 }
 
@@ -59,10 +112,18 @@ module.exports = function(app, db){
         })
         .delete(async (req, res) => {
             try {
-                let playlist = await db.Playlist.find({where: {id: req.params.id}})
+                let playlist = await db.Playlist.findOne({where: {id: req.params.id}})
 
                 if (playlist != null){
                     await playlist.destroy();
+
+                    // Delete all records of PlaylistSongs with this playlist id
+                    let songs = await db.PlaylistSongs.findAll({where: {playlistId: playlist.id}});
+                                        
+                    for (i = 0; i < songs.length; i++){
+                        await songs[i].destroy();
+                    }
+
                     console.log("Successfully DELETE playlist: " + req.params.id);
                 }
 
